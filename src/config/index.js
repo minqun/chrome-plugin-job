bc825208ca6879000635c6fa83ef43e72399b076
M.re <c1029mq@qq.com>
2022-09-01 14:21:42 +0800

    ls

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@
  * @Author: M.re c1029mq@qq.com
  * @Date: 2022-06-20 10:09:16
  * @LastEditors: M.re c1029mq@qq.com
- * @LastEditTime: 2022-08-10 14:26:12
+ * @LastEditTime: 2022-08-30 21:06:57
  * @FilePath: /erp-plugin/src/config/index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
